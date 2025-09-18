0f84169751144351db872c6a74d68e7af753fb21
ResArt2005 <resart2005@gmail.com>
2025-09-18 12:50:20 +0400

    fix

--- app_ai/events/events.py
+++ app_ai/events/events.py
@@ -1,4 +1,3 @@
-import gradio as gr
 from components.chat_utils import (
     clear_current_chat, fetch_llm_answer, new_chat, switch_chat, rename_chat, format_message, sync_chat_list, add_user_message
 )
