59899e044c040d317365b461d5adf486fcae7a23
ResArt2005 <resart2005@gmail.com>
2025-11-24 14:03:31 +0400

    fix

--- app_ai/config/config.py
+++ app_ai/config/config.py
@@ -9,6 +9,6 @@ class Config:
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_HOST = os.getenv("DB_HOST")
-    DB_PORT = int(os.getenv("DB_PORT", 5432))
+    DB_PORT = int(os.getenv("DB_PORT", 5433))
     API_KEY = os.getenv("API_KEY")
     DEBUG = os.getenv("DEBUG", "False").lower() == "true"
