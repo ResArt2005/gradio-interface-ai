e73dd83168a73535e18094f1c1647451961aaf11
ResArt2005 <resart2005@gmail.com>
2025-12-08 13:05:21 +0400

    fix

--- app_ai/tools/dbpg/sql/chats.sql
+++ app_ai/tools/dbpg/sql/chats.sql
@@ -4,5 +4,8 @@ CREATE TABLE IF NOT EXISTS chats (
     title VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    msg_count INTEGER NOT NULL DEFAULT 0
+    msg_count INTEGER NOT NULL DEFAULT 0,
+	chat_logs jsonb NULL,
+	chat_state varchar(8) DEFAULT 'active'::character varying NULL,
+	CONSTRAINT chats_chat_state_check CHECK ((((chat_state)::text = 'active'::text) OR ((chat_state)::text = 'disabled'::text)))
 );
